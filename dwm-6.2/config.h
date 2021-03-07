e729d6140e9c8dd6e8fd4ed525327f8f2c0a9b35
MrsHerobrine (Naomi) <57451966+mrsherobrine@users.noreply.github.com>
2021-03-07 09:58:52 +0100

    edited config.h to allow for pywal colours

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -8,19 +8,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#3498db";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+#include "/home/naom/.cache/wal/colors-wal-dwm.h"
+
+
+
 
 static const char *const autostart[] = {
-	"xrdb", "/home/naom/.Xresources", NULL,
 	"slstatus", NULL,
 	"picom", NULL,
 	"feh", "--bg-fill", "/home/naom/Pictures/backgrounds/cropped-1920-1080-1050844.jpg", NULL,
@@ -67,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
 static const char *screencmd[] = { "xfce4-screenshooter", "-r", NULL };
 
@@ -100,6 +93,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
