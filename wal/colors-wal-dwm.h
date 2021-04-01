ae59f1f5cad4b5bfc92d8652d4ae4e7f622f48b8
MrsHerobrine (Naomi) <57451966+mrsherobrine@users.noreply.github.com>
2021-04-01 15:53:55 +0200

    removed code that wasnt used

--- wal/colors-wal-dwm.h
+++ wal/colors-wal-dwm.h
@@ -6,13 +6,8 @@ static const char sel_fg[] = "#c4c0c6";
 static const char sel_bg[] = "#8E706B";
 static const char sel_border[] = "#c4c0c6";
 
-static const char urg_fg[] = "#c4c0c6";
-static const char urg_bg[] = "#6B6E70";
-static const char urg_border[] = "#6B6E70";
-
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
     [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
 };
